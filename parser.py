0be340247bb007849b4d408eccac26a5b59ca526
hddn <hddnsmile@gmail.com>
2017-05-30 15:13:41 +0300

    Add extra tries for fetching page and status messages

--- parser.py
+++ parser.py
@@ -28,74 +28,110 @@ def clear_string(text):
 
 def parse_hda(url, header, filename):
     """Parse home-draw-away page. Works for 1st/2nd time also"""
-    browser = webdriver.PhantomJS()
-    browser.get(url)
-    time.sleep(4)
-    soup = BeautifulSoup(browser.page_source, 'lxml')
-    browser.close()
 
-    bookies = [b.span.text.strip() for b in soup.select('.BookieLogo')]
+    def fetch_page():
+        for i in range(3):
+            print('Fetching page...')
+            browser = webdriver.PhantomJS()
+            browser.get(url)
+            time.sleep(4)
+            soup = BeautifulSoup(browser.page_source, 'lxml')
+            browser.close()
+            if soup.select('.BookieLogo'):
+                return soup
 
-    if 'Betdaq' in bookies:
-        betdaq = bookies.pop()
-        bookies = bookies + AHP + [betdaq]
+    page = fetch_page()
+    if page:
+        print('Parsing data...')
+
+        bookies = [b.span.text.strip() for b in page.select('.BookieLogo')]
+        if 'Betdaq' in bookies:
+            betdaq = bookies.pop()
+            bookies = bookies + AHP + [betdaq]
+        else:
+            bookies = bookies + AHP
+
+        payouts = [p.text.strip() for p in page.select('.Payout')]
+
+        hda = []
+        for row in page.select('.OTOddsData')[0].find_all(class_='OTRow'):
+            hda.append([clear_string(r.text) for r in row.select('li')])
+
+        print('Done')
+        print('Saving data to csv file...')
+
+        with open('{}.csv'.format(filename), 'w') as r:
+            writer = csv.writer(r)
+            writer.writerow(header)
+            result = list(zip_longest(bookies, payouts, hda))
+            for row in result:
+                writer.writerow([row[0], row[1], row[2][0], row[2][1], row[2][2]])
+        print('{} records processed'.format(str(len(result))))
     else:
-        bookies = bookies + AHP
-
-    payouts = [p.text.strip() for p in soup.select('.Payout')]
-
-    hda = []
-    for row in soup.select('.OTOddsData')[0].find_all(class_='OTRow'):
-        hda.append([clear_string(r.text) for r in row.select('li')])
-
-    with open('{}.csv'.format(filename), 'w') as r:
-        writer = csv.writer(r)
-        writer.writerow(header)
-        for row in list(zip_longest(bookies, payouts, hda)):
-            writer.writerow([row[0], row[1], row[2][0], row[2][1], row[2][2]])
+        print('An error occurred. (Check if link is still alive or try again)')
+        return
 
 
 def parse_asian_or_overunder(url, header, filename):
     """Parse asian-handicap or over-under page. Works for 1st/2nd half also"""
-    browser = webdriver.PhantomJS()
-    browser.get(url)
-    time.sleep(4)
-    links = browser.find_elements_by_class_name('CollapsibleTitle')
-    for l in links:
-        l.click()
 
-    soup = BeautifulSoup(browser.page_source, 'lxml')
-    browser.close()
+    def fetch_page():
+        for i in range(3):
+            print('Fetching page...')
+            browser = webdriver.PhantomJS()
+            browser.get(url)
+            time.sleep(4)
 
-    result = []
-    for box in soup.select('.OutcomeBox'):
-        result.append([box.select('.CollapsibleTitle')[0].text])
-        for row in box.find_all('ul', class_='OTRow'):
-            result.append([clear_string(r.text.strip()) for r in row.find_all('li', class_='OTCol', recursive=False)])
+            links = browser.find_elements_by_class_name('CollapsibleTitle')
+            for l in links:
+                l.click()
 
-    def pretty_result():
-        """Split bookies and payouts to different columns"""
-        for res in result:
-            if '%' in res[0]:
-                bookie, payout = res[0][:-3], res[0][-3:]
-                if payout == '00%':
-                    bookie, payout = res[0][:-4], res[0][-4:]
-                res[0] = bookie
-                res.insert(1, payout)
-            elif ')' in res[0]:
-                res.insert(0, None)
-                res.insert(0, 'Betdaq')
-            else:
-                res.insert(1, None)
-        return result
+            soup = BeautifulSoup(browser.page_source, 'lxml')
+            browser.close()
+            if soup.select('.OutcomeBox'):
+                return soup
 
-    result = pretty_result()
+    page = fetch_page()
+    if page:
+        print('Parsing data...')
 
-    with open('{}.csv'.format(filename), 'w') as r:
-        writer = csv.writer(r)
-        writer.writerow(header)
-        for row in result:
-            writer.writerow(row)
+        result = []
+        for box in page.select('.OutcomeBox'):
+            result.append([box.select('.CollapsibleTitle')[0].text])
+            for row in box.find_all('ul', class_='OTRow'):
+                result.append([clear_string(r.text.strip()) for r in row.find_all('li', class_='OTCol',
+                                                                                  recursive=False)])
+
+        def pretty_result():
+            """Split bookies and payouts to different columns"""
+            for res in result:
+                if '%' in res[0]:
+                    bookie, payout = res[0][:-3], res[0][-3:]
+                    if payout == '00%':
+                        bookie, payout = res[0][:-4], res[0][-4:]
+                    res[0] = bookie
+                    res.insert(1, payout)
+                elif ')' in res[0]:
+                    res.insert(0, None)
+                    res.insert(0, 'Betdaq')
+                else:
+                    res.insert(1, None)
+            return result
+
+        result = pretty_result()
+
+        print('Done')
+        print('Saving data to csv file...')
+
+        with open('{}.csv'.format(filename), 'w') as r:
+            writer = csv.writer(r)
+            writer.writerow(header)
+            for row in result:
+                writer.writerow(row)
+        print('{} records processed'.format(str(len(result))))
+    else:
+        print('An error occurred. (Check if link is still alive or try again)')
+        return
 
 
 def main():
@@ -107,9 +143,11 @@ def main():
     elif 'asian-handicap' in link:
         header = HEADER.get('asian_or_over')
         parse_asian_or_overunder(url=link, header=header, filename='asian-handicap')
-    elif 'over-under'in link:
+    elif 'over-under' in link:
         header = HEADER.get('asian_or_over')
         parse_asian_or_overunder(url=link, header=header, filename='over-under')
+    else:
+        print('Please, provide a valid url')
 
 
 if __name__ == '__main__':
